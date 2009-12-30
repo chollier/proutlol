3771ff708c694f0d3c68b1891d845524ff938404
Loïc CHOLLIER <loic@chollier.com>
2009-12-30 15:38:52 +0100

    bordel de merde de facebooker qui me niquait tout, rollback et pensez à changer ma secret key parceque bon.

--- db/schema.rb
+++ db/schema.rb
@@ -12,80 +12,33 @@
 ActiveRecord::Schema.define(:version => 20091022141733) do
 
   create_table "comments", :force => true do |t|
-    t.string   "url",        :limit => 250
+    t.string   "name"
+    t.string   "email"
+    t.string   "url"
     t.text     "content"
     t.integer  "post_id"
     t.integer  "user_id"
+    t.string   "ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name",       :limit => 250
-    t.string   "email",      :limit => 250
   end
-
-  create_table "contents", :force => true do |t|
-    t.string   "type"
-    t.string   "title"
-    t.string   "author"
-    t.text     "body"
-    t.text     "extended"
-    t.text     "excerpt"
-    t.string   "keywords"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id"
-    t.string   "permalink"
-    t.string   "guid"
-    t.integer  "text_filter_id"
-    t.text     "whiteboard"
-    t.string   "name"
-    t.boolean  "published",      :default => false
-    t.boolean  "allow_pings"
-    t.boolean  "allow_comments"
-    t.datetime "published_at"
-    t.string   "state"
-  end
-
-  add_index "contents", ["published"], :name => "index_contents_on_published"
-  add_index "contents", ["text_filter_id"], :name => "index_contents_on_text_filter_id"
-
-  create_table "feedback", :force => true do |t|
-    t.string   "type"
-    t.string   "title"
-    t.string   "author"
-    t.text     "body"
-    t.text     "excerpt"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id"
-    t.string   "guid"
-    t.integer  "text_filter_id"
-    t.text     "whiteboard"
-    t.integer  "article_id"
-    t.string   "email"
-    t.string   "url"
-    t.string   "ip",               :limit => 40
-    t.string   "blog_name"
-    t.boolean  "published",                      :default => false
-    t.datetime "published_at"
-    t.string   "state"
-    t.boolean  "status_confirmed"
-  end
-
-  add_index "feedback", ["article_id"], :name => "index_feedback_on_article_id"
-  add_index "feedback", ["text_filter_id"], :name => "index_feedback_on_text_filter_id"
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "content"
     t.integer  "user_id"
+    t.string   "permalink"
+    t.string   "facebook_title"
+    t.text     "facebook_description"
+    t.string   "facebook_image"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "permalink"
   end
 
   create_table "shares", :force => true do |t|
     t.integer  "type"
     t.integer  "creator_id"
+    t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
